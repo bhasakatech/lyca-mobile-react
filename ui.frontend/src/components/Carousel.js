f28f1d6fdf22cf16c945ff9a56d9ba4f75d1a336
venky16745 <venkatnagendravenky@gmail.com>
2025-08-28 13:03:52 +0530

    corousal Component integration

--- ui.frontend/src/components/Carousel.js
+++ ui.frontend/src/components/Carousel.js
@@ -1,43 +1,21 @@
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 
 
-function ControlledCarousel() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
-
+const ControlledCarousel = ({ carouselImages = [] }) => {
   return (
-    <Carousel
-     activeIndex={index} onSelect={handleSelect}
-     indicators={true}   
-      controls={true} 
-     >
-
-      <Carousel.Item>
-       <img
-            src="/content/dam/lyca-mobile/assets/WhatsApp%20Image%202025-08-23%20at%203.49.07%20PM.jpeg"
-            alt=""
-          />
-      </Carousel.Item>
-      <Carousel.Item>
+    <Carousel indicators={true} controls={true}>
+      {carouselImages.map((img, index) => (
+        <Carousel.Item key={index}>
           <img
-            src="/content/dam/lyca-mobile/assets/WhatsApp%20Image%202025-08-23%20at%203.49.07%20PM.jpeg"
-            alt=""
+            src={img}
+            className="d-block w-100"
           />
-      </Carousel.Item>
-      <Carousel.Item>
-          <img
-            src="/content/dam/lyca-mobile/assets/WhatsApp%20Image%202025-08-23%20at%203.49.07%20PM.jpeg"
-            alt=""
-          />
-      </Carousel.Item>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
-}
+};
 
 export default ControlledCarousel;
