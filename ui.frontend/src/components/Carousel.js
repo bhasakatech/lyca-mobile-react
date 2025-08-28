9246791f2d2e48ed3026bc3a1b278b2475fd0047
nitheesh2929 <nitheesh.r@tychons.com>
2025-08-28 15:38:25 +0530

    Carousel Integration bug fixed

--- ui.frontend/src/components/Carousel.js
+++ ui.frontend/src/components/Carousel.js
@@ -2,15 +2,20 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from "react-bootstrap/Carousel";
 
-
 const ControlledCarousel = ({ carouselImages = [] }) => {
   return (
-    <Carousel indicators={true} controls={true}>
+    <Carousel 
+      indicators={true} 
+      controls={true} 
+      interval={3000}
+      pause={false} 
+    >
       {carouselImages.map((img, index) => (
         <Carousel.Item key={index}>
           <img
             src={img}
             className="d-block w-100"
+            alt={`slide-${index}`}
           />
         </Carousel.Item>
       ))}
